5dfa44bf2ca596f8da1eaf12937e4a2c5992cebb
Garima Parihar <garima.parihar_cs19@gla.ac.in>
2020-08-30 18:06:06 +0530

    add field bookName for the Book.java class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,11 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
+ *  User: Garima Singh Parihar (191500287)
+ *  Date: 30/08/20
  *  Time: 3:49 PM
  *  File Name : Book.java
  * */
+
 package definitions;
 
 public class Book {
-}
+    private String bookName;
