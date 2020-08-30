135d32754e3ce95a3819146b550414105d5b71b1
Garima Parihar <garima.parihar_cs19@gla.ac.in>
2020-08-30 18:33:45 +0530

    add a method to get the name of the book.

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -11,4 +11,22 @@ public class Book {
     private String bookName;
     private String bookAuthorName;
     private String thirteenDigitISBNNumber;
+
+    /**
+     * This method returns book name , author name , book ISBN number .
+     *
+     * @param bookName                This return name of the book .
+     * @param bookAuthorName          This return book author name of the book .
+     * @param thirteenDigitISBNNumber This return ISBN number of the book .
+     */
+    public Book(String bookName, String bookAuthorName, String thirteenDigitISBNNumber) {
+        this.bookName = bookName;
+        this.bookAuthorName = bookAuthorName;
+        this.thirteenDigitISBNNumber = thirteenDigitISBNNumber;
+    }
+
 }
+
+
+
+
