7de1eff2ab33262682da988bb80685ffe2d9470b
Garima Parihar <garima.parihar_cs19@gla.ac.in>
2020-08-30 20:12:16 +0530

    add scanner class and take user input.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -8,6 +8,8 @@ package execution;
 
 import definitions.Library;
 
+import java.util.Scanner;
+
 
 public class FrontDesk {
     private static final int ISSUE_BOOK = 1;
@@ -17,6 +19,52 @@ public class FrontDesk {
     private Library[] studentDetailsThoseAreCurrentlyInteracting;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int clientInput;
+        String nameOfTheStudent;
+        long universityRollNumber;
+        Library myLibrary = new Library();
+        do {
+            System.out.println("_=_=__=_=_Welcome To the Front Desk_=_=__=_=_");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return the previously issues books");
+            System.out.println("3. Show all previously issue books");
+            System.out.println("4. Exit");
+
+            clientInput = scanner.nextInt();
+            switch (clientInput) {
+                case ISSUE_BOOK:
+                    System.out.println("enter your name");
+                    scanner.nextLine();
+                    studentName = scanner.next();
+                    System.out.println("enter your university Rollno");
+                    universityRollNumber = scanner.nextLong();
+                    System.out.println("Enter the name of the book that you want to issue");
+                    scanner.nextLine();
+                    String bookName = scanner.nextLine();
+                    student.doIssueBook(bookName);
+                    break;
+                case RETURN_BOOK:
+                    System.out.println("enter your name");
+                    studentName = scanner.next();
+                    scanner.nextLine();
+                    System.out.println("enter your university Rollno");
+                    universityRollNumber = scanner.nextLong();
+                    System.out.println("Enter the name of the book that you want to return");
+                    scanner.nextLine();
+                    bookName = scanner.nextLine();
+                    student.doReturnBook(bookName);
+                    break;
+                case LIST_INVENTORY:
+                    student.listInventory();
+                    break;
+                default:
+                    System.out.println("WRONG CHOICE");
+            }
+        } while (clientInput != EXIT);
+        scanner.close();
+    }
 
 
     }
