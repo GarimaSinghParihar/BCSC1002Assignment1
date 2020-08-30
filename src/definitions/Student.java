e0f14a98bed5506edee069c13303fcde124e6c38
Garima Parihar <garima.parihar_cs19@gla.ac.in>
2020-08-30 19:56:03 +0530

    add getter and setter for the name of student.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,8 +7,32 @@
 package definitions;
 
 public class Student {
-    private String[] nameOfStudent;
+    private String[] nameOfStudentFormatFirstMiddleLast;
     private long universityRollNumber;
     private int numberOfBooksIssued;
     private Book[] nameOfBooksIssued;
+
+    public Student(String nameOfTheStudent, long universityRollNo, int numberOfBooksIssued) {
+        this.nameOfStudentFormatFirstMiddleLast = nameOfTheStudent.split(" ");
+        this.universityRollNumber = universityRollNo;
+        this.numberOfBooksIssued = numberOfBooksIssued;
+        this.nameOfBooksIssued = new Book[numberOfBooksIssued];
+    }
+
+    /**
+     * this method return the name of the student (first name, middle name, last name- all String).
+     *
+     * @return the name of the student (first name, middle name, last name- all String).
+     */
+
+    public String[] getNameOfStudentFormatFirstMiddleLast() {
+        return nameOfStudentFormatFirstMiddleLast;
+    }
+
+    public void setNameOfStudentFormatFirstMiddleLast(String[] nameOfStudentFormatFirstMiddleLast) {
+        this.nameOfStudentFormatFirstMiddleLast = nameOfStudentFormatFirstMiddleLast;
+    }
+
+
 }
+
